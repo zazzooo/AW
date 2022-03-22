a941c6a9ae9301b9ba7445cce829ba982649959c
zazzooo <lorenzo.gini1998@gmail.com>
2022-03-22 12:18:46 +0100

    Add first report try
    There are still few issue, the temp folder is not temporary at all. Every html has to have a different name and it has to be called by the shell.

--- html_report_python.py
+++ html_report_python.py
@@ -4,9 +4,15 @@ import pandas as pd
 
 table_headers = ['index','value']
 
-def create_page(df_stats):
-    doc = dominate.document(title = 'Tabble')
+def create_page(df_stats, list_securities):
+    doc = dominate.document(title = 'Report')
 
+    #compute name to append at the name of the figure
+    unique_name = ''
+    for i in list_securities:
+        unique_name += '_' + i
+    
+    #start to create the html file
     with doc.head:
         link(rel = 'stylesheet', href = 'style.css')
         style("""table, th, td {border:1px solid black;) """)
@@ -25,23 +31,30 @@ def create_page(df_stats):
                             td(round(df_stats.iloc[i]['value'],4))
 
         with div(cls='container', style='display:inline-block;margin-top: 50px;'):
-            img(src = "./img/table_montly_returns.png", alt = 'Distribution plot' ,width="550", height="330")
+            img(src = "./img/temp/table_montly_returns" + unique_name + ".png", alt = 'Distribution plot' ,width="550", height="330")
 
         with div(cls='container', style='display:inline-block;margin-top: 50px;'):
-            img(src = "./img/distribution_returns.png", alt = 'Distribution plot' ,width="420", height="288")
+            img(src = "./img/temp/distribution_returns" + unique_name + ".png", alt = 'Distribution plot' ,width="480", height="350")
 
         with div(cls='container', style='display:inline-block;vertical-align:top;margin-top: 50px;margin-left: 50px;'):
-            img(src = "./img/drawdowns_period.png", alt = 'Distribution plot', width="576", height="288")
+            img(src = "./img/temp/drawdowns_period" + unique_name + ".png", alt = 'Distribution plot', width="576", height="288")
 
         with div(cls='container', style='display:inline-block;margin-top: 50px;'):
-            img(src = "./img/under_water_plot.png", alt = 'Distribution plot' ,width="576", height="288")
+            img(src = "./img/temp/under_water_plot" + unique_name + ".png", alt = 'Distribution plot' ,width="576", height="288")
 
 
 
     print(doc)
 
-df = pd.read_csv('./data/statistics_all_weather.csv')
-create_page(df)
+
+list_securities = ['SSO','UBT', 'UST', 'UGL', 'DIG']
+
+unique_name = ''
+for i in list_securities:
+    unique_name += '_' + i
+df = pd.read_csv('./data/temp/statistics_all_weather'+ unique_name +'.csv')
+
+create_page(df, list_securities)
 
 
 #To create the html file call from the shell (in the right directory) $pyhton3 html_with_python.py >> report.html  This command will create a report.html file in the same directory with the report of the strategy
